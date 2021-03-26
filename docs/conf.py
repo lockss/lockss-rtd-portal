79de835c8aaeb6f620f78de5ab2950b93e8b5e13
Thib Guicherd-Callin <thib@cs.stanford.edu>
2021-03-26 11:54:58 -0700

    Hide "On Read The Docs" and "On GitHub" sections of the versions tab

--- docs/conf.py
+++ docs/conf.py
@@ -64,6 +64,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# See https://docs.readthedocs.io/en/stable/guides/adding-custom-css.html
+html_css_files = [
+    'css/lockss.css',
+]
+
 # See https://github.com/readthedocs/readthedocs.org/issues/2569
 master_doc = 'index'
 
